cfdddc5e33719e7fb8f9c598b717c39f42401379
siddharth733 <siddharthschauhan694@gmail.com>
2022-05-07 13:46:06 +0530

    save

--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -77,12 +77,13 @@ const Navbar = () => {
         )
     }
 
+
     return (
         <>
             <nav className="navbar navbarFirst navbar-expand-lg navbar-light shadow sticky-top">
                 <div className="container ">
                     <Link className="NavHead" to="#">Book Store</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
